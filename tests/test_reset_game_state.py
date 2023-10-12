0d6ee3fd7e2195be41bedc7bcf363acef939bc4f
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2023-10-13 00:42:45 +0100

    reset state tests completed.

--- tests/test_reset_game_state.py
+++ tests/test_reset_game_state.py
@@ -5,14 +5,20 @@ from src.main import reset_game_state
 
 
 class ResetGameState(unittest.TestCase):
-    # # As generate_random_word connects to an API, we mock it out to avoid
+    # As generate_random_word connects to an API, we mock it out to avoid
     # spamming the API and providing more accuracy if something is maybe wrong
     # with the API.
     # Also providing more speed.
     def setUp(self):
         self.unfinished_word = ["_", "_", "_", "_", "_", "_", "_"]
+
+        # Start the patcher for the generate_random_word function call
+        self.mock_generate_random_word = patch("src.main.generate_random_word").start()
+        # Set the return value of the mocked function call (generate_random_word)
+        self.mock_generate_random_word.return_value = "hardcoded"
+
         self.reset_game = reset_game_state()
-        self.new_word = "mocked"
+        self.new_word = "hardcoded"
 
     def test_reset_game_state_unfinished_word(self):
         self.assertEqual(
@@ -24,17 +30,10 @@ class ResetGameState(unittest.TestCase):
 
     def test_reset_game_state_new_word(self):
         reset_game = reset_game_state()
-        self.assertEqual(reset_game["new_word"], "mocked")
+        self.assertEqual(reset_game["new_word"], self.new_word)
 
-    @patch("src.main.generate_random_word")
-    def test_reset_game_state(self, mock_generate_random_word):
-        # Set the return value of the mocked function
-        mock_generate_random_word.return_value = "hardcoded"
-
-        game_state = reset_game_state()
-
-        # Now, whenever reset_game_state is called within this test, generate_random_word will return 'hardcoded'
-        self.assertEqual(game_state["new_word"], "hardcoded")
+    def tearDown(self):
+        patch.stopall()
 
 
 if __name__ == "__main__":
